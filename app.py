69af0db5ecd9eff0648a09b4f735e06344895960
vladek1934 <vlad.mun1999@gmail.com>
2019-02-19 15:51:36 +0600

    Forms update 2

--- app.py
+++ app.py
@@ -41,9 +41,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'admin':
+            flash('Welcome', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 

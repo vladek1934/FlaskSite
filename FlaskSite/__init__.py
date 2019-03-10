155de0c90c26ad9bb366c310651ecb2bb22c8795
vladek1934 <vlad.mun1999@gmail.com>
2019-03-10 12:03:36 +0600

    Migrated to 3.7.2

--- FlaskSite/__init__.py
+++ FlaskSite/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_redis import Redis
+from flask_redis import FlaskRedis
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-redis = Redis(app)
+redis = FlaskRedis(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
